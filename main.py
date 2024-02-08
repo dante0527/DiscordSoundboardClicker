cadc87b7490af6a1d4c3c3005f7550c03e4bfc9a
dante0527 <19dante.j.lee@gmail.com>
2024-02-07 22:37:39 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from pyautogui import moveTo, click
+from time import sleep
+
+for _ in range(1):
+    rows = 8
+    cols = 3
+
+    x_pos = 455 # 160
+    y_pos = 630 #50
+
+    moveTo(1, 1)
+
+    for _ in range(rows):
+        for _ in range(cols):
+
+            moveTo(x_pos, y_pos)
+            click()
+            sleep(1)
+
+            x_pos += 160
+
+        x_pos = 455
+        y_pos += 48
